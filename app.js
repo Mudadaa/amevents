d1d16133341f6ccbdd2243fb92b5846dd5004090
Mudadaa <layeb.hassan@gmail.com>
2023-09-05 01:02:17 +0200

    photos added to the galerie page layout in progress

---
+++ app.js
@@ -0,0 +1,10 @@
+const app={
+     galeriePhotos=document.querySelector("galerie-photos")
+
+
+
+     getHidePhotos:function(){
+
+     }
+}
+document.addEventListener('DOMContentLoaded', app.getHidePhotos);
