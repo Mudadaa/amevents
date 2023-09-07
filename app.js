668f729ecf1310ef2190543cc361b03a99ef2027
Mudadaa <layeb.hassan@gmail.com>
2023-09-07 22:51:16 +0200

    button to show photos working

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const app={
-     galeriePhotos=document.querySelector("galerie-photos")
-
-
-
-     getHidePhotos:function(){
-
-     }
-}
-document.addEventListener('DOMContentLoaded', app.getHidePhotos);
+ const app = {
+     init: function(){
+       app.galeriePhotos= document.querySelector(".galeriePhotos");
+       app.ButtonPhoto=document.querySelector(".photos");
+       app.ButtonPhoto.addEventListener("click", app.getPhotos);
+     }, 
+     getHidePhotos: function() { // Define the function
+          console.log("ça click oùùù");
+          app.galeriePhotos.removeAttribute("hidden");
+        },
+        getPhotos: function(){
+          app.galeriePhotos.setAttribute("hidden",true);
+          console.log("ça s'annule ouuu ?")
+        }
+   
+   }
+   document.addEventListener('DOMContentLoaded', app.init);
