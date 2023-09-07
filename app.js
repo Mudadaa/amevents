3b00866977dbea0cb0508dc47f1d0c590a284179
Mudadaa <layeb.hassan@gmail.com>
2023-09-08 01:03:48 +0200

    button to toggle photos&videos done

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
  const app = {
      init: function(){
+      //on récupère la galerie de photos
        app.galeriePhotos= document.querySelector(".galeriePhotos");
+      // on récup le boutton pour les photos
        app.ButtonPhoto=document.querySelector(".photos");
-       app.ButtonPhoto.addEventListener("click", app.getPhotos);
-     }, 
-     getHidePhotos: function() { // Define the function
-          console.log("ça click oùùù");
-          app.galeriePhotos.setAttribute("hidden",true);
-        },
-        getPhotos: function(){
-          app.galeriePhotos.removeAttribute("hidden");
-          
-          console.log("ça s'annule ouuu ?")
-        }
-   
-   }
+      // on ajoute au boutton l'évènement click et la fonction de toggle
+       app.ButtonPhoto.addEventListener("click", app.toggleHidePhotos);
+     
+       //idem pour la galerie vidéo, le boutton vidéo=>click=>fct
+       app.galerieVideos=document.querySelector(".galerieVideos");
+       app.ButtonVideo=document.querySelector(".videos");
+       app.ButtonVideo.addEventListener("click", app.toggleHideVideos);
+      }, 
+     toggleHidePhotos: function() { 
+     console.log("tof caché ou pas ?");
+    app.galeriePhotos.hidden =!app.galeriePhotos.hidden
+    },
+    toggleHideVideos: function(){
+console.log("video caché ou pas ?");
+app.galerieVideos.hidden=!app.galerieVideos.hidden
+    }
+  }
+
    document.addEventListener('DOMContentLoaded', app.init);
