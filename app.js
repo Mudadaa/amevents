1a3ba7e1af9cd6ac7037a544ba1feb3143a76ab9
Mudadaa <layeb.hassan@gmail.com>
2023-09-11 20:06:43 +0200

    debug toggle functions

--- app.js
+++ app.js
@@ -6,44 +6,40 @@
       // on récup le boutton pour les photos
        app.ButtonPhoto=document.querySelector(".photos");
       // on ajoute au boutton l'évènement click et la fonction de toggle
-       app.ButtonPhoto.addEventListener("click", app.toggleHidePhotos);
+       app.ButtonPhoto.addEventListener("click", app.togglePhotos);
      
        //idem pour la galerie vidéo, le boutton vidéo=>click=>fct
        app.galerieVideos=document.querySelector(".galerieVideos");
        app.ButtonVideo=document.querySelector(".videos");
-       app.ButtonVideo.addEventListener("click", app.toggleHideVideos);
+       app.ButtonVideo.addEventListener("click", app.toggleVideos);
       }, 
-     toggleHidePhotos: function() { 
-     console.log("tof caché ou pas ?");
-    
-    app.galeriePhotos.hidden =!app.galeriePhotos.hidden
-    app.galerieVideos.hidden
-    if(app.galerieVideos.hidden="false"){
-        app.galeriePhotos};
+
+//Pour le toggle caché, montré
+     togglePhotos: function() { 
+     
         // comme l'affichage de la page est mise sur flex
         //il faut le retirer car cela neutralise l'action de la fonction toggle
         //app.galeriePhotos.style.display = 'flex'; tt seul fige galeriePhoto en mode 'ouvert'
     if (app.galeriePhotos.style.display === 'flex') {
      app.galeriePhotos.style.display = 'none'; // Masquez la galerie de photos
+     app.galerieVideos.style.display='none';
     } else {
     app.galeriePhotos.style.display = 'flex'; // Affichez la galerie de photos en tant que flex
     };
-    
+   
     },
-    toggleHideVideos: function(){
-console.log("video caché ou pas ?");
+
+
+    toggleVideos: function(){
+
+//pour le display en flex 
 if(app.galerieVideos.style.display==='flex'){
-    app.galerieVideos.style.display='none'
+    app.galeriePhotos.style.display = 'none'; 
+    app.galerieVideos.style.display='none';
 }else{
-    app.galerieVideos.style.display='flex'
-}
-app.galerieVideos.hidden=!app.galerieVideos.hidden
-if(app.galerieVideos.hidden="false"){
-    app.galerieVideos
-}
+    app.galerieVideos.style.display='flex';
+};
 
-
- 
     }
   }
 
